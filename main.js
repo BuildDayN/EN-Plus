e1d9bb00f656027d1a719fcad7fdf58d7a8868b3
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-06-24 15:00:26 +0900

    Add milkcocoa API key

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-$(function() {
+$(function () {
+
+
     //1.ミルクココアインスタンスを作成
     var milkcocoa = new MilkCocoa("app_id.mlkcca.com");
+
+
+    var milkcocoa = MilkCocoa.connectWithApiKey('unijisd7eld.mlkcca.com', 'BCCBMCLADKJEDEOP', 'IiEilAPTODgMeJkXJDSRSCRjdTRCUGEYUOLfCEUK');
 
     //2."message"データストアを作成
     var ds = milkcocoa.dataStore("message");
@@ -9,60 +14,59 @@ $(function() {
     var windowHeight = $(window).height();
     var bodyHeight = $('body').height();
 
-    function recalcHeights(){
-      windowHeight = $(window).height();
-      bodyHeight = $('body').height();
+    function recalcHeights() {
+        windowHeight = $(window).height();
+        bodyHeight = $('body').height();
     }
 
-    $(window).on('resize', $.debounce( 200, function(){
+    $(window).on('resize', $.debounce(200, function () {
         recalcHeights();
-      })
-    );
+    }));
 
     function getData(callback) {
 
-      $(window).off('scroll');
+        $(window).off('scroll');
 
-      stream.next(function(err, elems) {
+        stream.next(function (err, elems) {
 
-        if(elems.length == 0){
-          callback();
-          return;
-        }
+            if (elems.length == 0) {
+                callback();
+                return;
+            }
 
-        var reversedElems = elems.reverse();
+            var reversedElems = elems.reverse();
 
-        reversedElems.forEach(function(el) {
-          renderMessage(el);
+            reversedElems.forEach(function (el) {
+                renderMessage(el);
+            });
+
+            recalcHeights();
+
+            $(window).on('scroll',
+                $.throttle(200, function () {
+                    if (($(window).scrollTop() + windowHeight) > (bodyHeight - 300)) getData(callback);
+                })
+            );
         });
-
-        recalcHeights();
-
-        $(window).on('scroll',
-          $.throttle( 200, function(){
-            if( ($(window).scrollTop() + windowHeight) > (bodyHeight - 300) ) getData(callback);
-          })
-        );
-      });
     }
 
-    getData(function(){
-      $("#messages").append('<div style="color: #DB6220; margin-top: 30px; text-align: center;">全データを読み込みました</div>');
+    getData(function () {
+        $("#messages").append('<div style="color: #DB6220; margin-top: 30px; text-align: center;">全データを読み込みました</div>');
     });
 
     //4."message"データストアのプッシュイベントを監視
-    ds.on("push", function(pushed) {
+    ds.on("push", function (pushed) {
         renderMessage(pushed, true);
     });
 
     function renderMessage(message, before) {
         var message_html = '<p class="post-text">' + escapeHTML(message.value.content) + '</p>';
         var date_html = '';
-        if(message.value.date) {
-            date_html = '<p class="post-date">'+escapeHTML( new Date(message.value.date).toLocaleString())+'</p>';
+        if (message.value.date) {
+            date_html = '<p class="post-date">' + escapeHTML(new Date(message.value.date).toLocaleString()) + '</p>';
         }
-        if(before) $("#messages div:first-child").before('<div id="'+message.id+'" class="post">'+message_html + date_html +'</div>');
-        else $("#messages").append('<div id="'+message.id+'" class="post">'+message_html + date_html +'</div>');
+        if (before) $("#messages div:first-child").before('<div id="' + message.id + '" class="post">' + message_html + date_html + '</div>');
+        else $("#messages").append('<div id="' + message.id + '" class="post">' + message_html + date_html + '</div>');
     }
 
     function post() {
@@ -83,7 +87,7 @@ $(function() {
         post();
     })
     $('#content').keydown(function (e) {
-        if (e.which == 13){
+        if (e.which == 13) {
             post();
             return false;
         }
